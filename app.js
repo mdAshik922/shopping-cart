ca0b7e80afc6d58cb3744194a97fb480e0c0f467
mdAshik922 <md.ashik0005723@gmail.com>
2022-11-03 12:38:27 +0600

    jsAdd

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 
 function shortCode(incressValue) {
     const intupValue = document.getElementById('input_Number');
-    const intupNumber = intupValue.value;
+    let intupNumber = intupValue.value;
     
     if (incressValue == true) {
-        intupValue.value = parseInt(intupNumber)+1;
+        intupNumber = parseInt(intupNumber)+1;
     }
     else if (intupNumber != 0) {
-        intupValue.value = parseInt(intupNumber)-1;
-    }
-}
+        intupNumber = parseInt(intupNumber)-1;
+    };
+    intupValue.value = intupNumber;
+
+    const phone = document.getElementById('price');
+    phone.innerText = intupNumber * 59;
+    
+};
 
 
 document.getElementById('fa_plus').addEventListener('click', function () {
-   shortCode(true)
+    shortCode(true);
     
 });
 document.getElementById('fa_minus').addEventListener('click', function () {
