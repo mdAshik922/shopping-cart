d5c2f833a48a03c3def0472f293cecb3142d9803
mdAshik922 <md.ashik0005723@gmail.com>
2022-11-03 12:12:24 +0600

    jsAdd

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ document.getElementById('fa_plus').addEventListener('click', function () {
     intupValue.value = parseInt(intupNumber)+1;
     
 });
+document.getElementById('fa_minus').addEventListener('click', function () {
+   const intupValue = document.getElementById('input_Number');
+    const intupNumber = intupValue.value;
+    
+    if (intupNumber != 0) {
+        intupValue.value = parseInt(intupNumber)-1;
+    }
+    
+});
