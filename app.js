145e53c6013eaaccddba12f2bf8a872c1c1324a1
mdAshik922 <md.ashik0005723@gmail.com>
2022-11-03 11:32:36 +0600

    jsAdd

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 document.getElementById('fa_plus').addEventListener('click', function () {
    const intupValue = document.getElementById('input_Number');
+    const intupValue_innerText = intupValue.innerText;
+    const innerText_press = parseFloat(intupValue_innerText);
     
 });
