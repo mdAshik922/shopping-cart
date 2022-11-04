d9c10fb2a6c40a916fe46d2c17be1152baa425a2
Mahmudul Hassan Ashik <62794453+mdAshik922@users.noreply.github.com>
2022-11-04 07:50:11 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-
-function shortCode(incressValue) {
-    const intupValue = document.getElementById('input_Number');
+function shortCode(product, price, incressValue) {
+    const intupValue = document.getElementById(product+'_input');
     let intupNumber = intupValue.value;
     
     if (incressValue == true) {
@@ -11,17 +10,27 @@ function shortCode(incressValue) {
     };
     intupValue.value = intupNumber;
 
-    const phone = document.getElementById('price');
-    phone.innerText = intupNumber * 59;
+    const phone = document.getElementById(input+'price');
+    phone.innerText = intupNumber * price;
     
 };
 
+//phone handel increase and decrease
+document.getElementById('phone_increase').addEventListener('click',function(){
+   shortCode('phone',1219,true);                  
+                        
+});
+
+document.getElementById('phone_decrease').addEventListener('click',function(){
+   shortCode('phone',1219,false);
+    
+});
 
 document.getElementById('fa_plus').addEventListener('click', function () {
-    shortCode(true);
+   shortCode('case',59,true);
     
 });
 document.getElementById('fa_minus').addEventListener('click', function () {
-    shortCode(false);
-    
+  shortCode('case',59,false);
+     
 });
